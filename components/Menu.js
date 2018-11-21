d295cc422e797a95728badd1a3341945d6a2c9cb
Brandon <bmc2775@gmail.com>
2018-11-21 17:34:55 -0500

    cleanup

--- components/Menu.js
+++ components/Menu.js
@@ -52,18 +52,6 @@ class Menu extends React.Component {
             <Link href="/sandboxes">
               <a className="navbar-item">sandbox</a>
             </Link>
-            
-
-            {/* <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">More</a>
-              <div className="navbar-dropdown">
-                <a className="navbar-item">About </a>
-                <a className="navbar-item">Jobs</a>
-                <a className="navbar-item">Contact</a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">Report an issue</a>
-              </div>
-            </div> */}
           </div>
         </div>
       </nav>)
--- layouts/Layout.js
+++ layouts/Layout.js
@@ -1,12 +1,6 @@
 import Menu from '../components/Menu'
 import Head from 'next/head'
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
-}
-
 const Layout = (props) => (
   <>
     <Head>
