8a727beed6645b95d0f190a2e0d27880a2322c86
Brandon <bmc2775@gmail.com>
2018-12-19 12:47:44 -0500

    updating assets

--- components/Menu.js
+++ components/Menu.js
@@ -17,8 +17,16 @@ class Menu extends React.Component {
     const { route } = this.props;
     const heroClass =
       route === "/"
-        ? "hero is-primary is-medium has-text-centered"
-        : "hero is-primary is-small has-text-centered";
+        ? "hero is-primary is-medium has-text-centered is-bold"
+        : "hero is-primary is-small has-text-centered is-bold";
+    const heroContent =
+      route === "/"
+        ? "title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile typewriter"
+        : "title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile";
+    const heroSubContent =
+      route === "/"
+        ? "subtitle is-size-4-desktop typewriter"
+        : "subtitle is-size-4-desktop";
     return (
       <section className={heroClass}>
         <div className="hero-head">
@@ -30,11 +38,14 @@ class Menu extends React.Component {
             <div className="navbar-brand">
               <Link href="/">
                 <a className="navbar-item">
-                  {/* <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" /> */}
-                  <div>bcox.tech</div>
+                  <img
+                    src="./../static/bcox_tech.svg"
+                    alt="[ bcox ]"
+                    width="112"
+                    height="28"
+                  />
                 </a>
               </Link>
-
               <a
                 role="button"
                 className="navbar-burger burger"
@@ -58,7 +69,9 @@ class Menu extends React.Component {
               <div className="navbar-end">
                 <Link href="/blog">
                   <a onClick={this.toggleNav} className="navbar-item">
-                    blog
+                    <div>
+                      <span className="underline">blog</span>
+                    </div>
                   </a>
                 </Link>
                 <Link href="/static/brandoncox_resume_201808.pdf">
@@ -68,7 +81,9 @@ class Menu extends React.Component {
                     onClick={this.toggleNav}
                     rel="noopener noreferrer"
                   >
-                    resume
+                    <div>
+                      <span className="underline">resume</span>
+                    </div>
                   </a>
                 </Link>
               </div>
@@ -80,12 +95,10 @@ class Menu extends React.Component {
           <div className="container">
             <div className="columns is-centered">
               <div className="column">
-                <h1 className="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">
+                <h1 className={heroContent}>
                   Engineer, Traveler, &amp; Techie
                 </h1>
-                <h2 className="subtitle is-size-4-desktop">
-                  Writing code and stuff.
-                </h2>
+                <h2 className={heroSubContent}>Writing code and stuff.</h2>
               </div>
             </div>
           </div>
